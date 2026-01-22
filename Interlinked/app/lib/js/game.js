7ed81389b54b2021c96ab1ec8dd3150e609fe8f5
Logan Nelson <Lnelson0915@gmail.com>
2026-01-21 18:33:15 -0800

    Add initial implementations for Currency, Game, and User classes with placeholder methods

---
+++ Interlinked/app/lib/js/game.js
@@ -0,0 +1,38 @@
+class Game {
+    score;
+    status;
+    price;
+    date;
+    players;
+
+    getScore() {
+        // TODO: Implement getScore method
+    }
+    setScore(score) {
+        // TODO: Implement setScore method
+    }
+    getStatus() {
+        // TODO: Implement getStatus method
+    }
+    setStatus(status) {
+        // TODO: Implement setStatus method
+    }
+    getPrice() {
+        // TODO: Implement getPrice method
+    }
+    setPrice(price) {
+        // TODO: Implement setPrice method
+    }
+    getDate() {
+        // TODO: Implement getDate method
+    }
+    setDate(date) {
+        // TODO: Implement setDate method
+    }
+    getPlayers() {
+        // TODO: Implement getPlayers method
+    }
+    setPlayers(players) {
+        // TODO: Implement setPlayers method
+    }
+}
---
+++ docs/analysis/nouns-and-verbs.md
@@ -0,0 +1,40 @@
+# This was made by Logan Nelson
+## Part A: Nouns and Verbs (Analysis)
+
+## Entities
+- Game
+  - Verbs: Upload, Download
+- Currency
+  - Verbs: acquire, spend, donate
+
+## Roles/Actors
+- Users
+  - Verbs: register, login, play, read, chat
+- Administrator
+  - Verbs: manage, ban, add
+
+## Attributes
+- Score
+- Date
+- Status
+- Price
+- Username
+- Email Address
+- User ID
+- Profile Picture
+- Balance
+- First Name
+- Last Name
+- Players
+
+## System/Technical
+- database
+- login page
+- registration page
+- profile page
+- button
+- screen
+- cursor
+
+## Part B: Early Object Structure (Code)
+
