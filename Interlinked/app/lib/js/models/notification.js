8d0071d56646001146e1e21e7dc4fdb38dda320a
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2026-01-26 17:26:26 -0800

    Adding Skeleton Code

---
+++ Interlinked/app/lib/js/models/notification.js
@@ -0,0 +1,48 @@
+class notification {
+    message
+    isRead
+    timeSent
+    reward
+    color
+    icon
+
+    setMessage(message) {
+        // TODO: implement setMessage() method
+    }
+
+    markAsRead() {
+        // TODO: implement markAsRead() method
+    }
+
+    setTimeSent(time) {
+        // TODO: implement setTimeSent() method
+    }
+    getTimeSent() {
+        // TODO: implement getTimeSent() method
+    }
+
+    setReward(reward) {
+        // TODO: implement setReward() method
+    }
+    getReward() {
+        // TODO: implement getReward() method
+    }
+
+    setColor(color) {
+        // TODO: implement setColor() method
+    }
+    getColor() {
+        // TODO: implement getColor() method
+    }
+
+    setIcon(icon) {
+        // TODO: implement setIcon() method
+    }
+    getIcon() {
+        // TODO: implement getIcon() method
+    }
+
+    toString() {
+        // TODO: implement toString() method
+    }
+}
